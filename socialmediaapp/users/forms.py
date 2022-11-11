9ad50259423fe04d9336ab9a7fcd85440a332f88
sehmuzc <115549234+sehmuzc@users.noreply.github.com>
2022-11-12 00:28:32 +0300

    Profile Update Function has been added for users
    Users will be able to update first name
     last name and e-mail address. Username will not be updated as it is a login credential.

--- socialmediaapp/users/forms.py
+++ socialmediaapp/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from.models import Profile
 from django.utils.translation import gettext_lazy as _
 
 class UserRegisterForm(UserCreationForm):
@@ -9,7 +10,18 @@ class UserRegisterForm(UserCreationForm):
     first_name = forms.CharField(label='First Name', min_length=1, max_length=50)
     last_name = forms.CharField(label='Last Name', min_length=1, max_length=50)
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput)  
-    
+
     class Meta:
             model = User
             fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+          email = forms.EmailField()
+          class Meta:
+              model = User
+              fields = ['email', 'first_name', 'last_name']
+
+class ProfileUpdateForm (forms.ModelForm):
+            class Meta:
+                model = Profile
+                fields = ['image']
