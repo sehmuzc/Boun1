084f906184a3f56321c8f8b8b0c348733fff7f0e
sehmuzc <115549234+sehmuzc@users.noreply.github.com>
2022-11-29 21:32:46 +0300

    Update settings.py

--- socialmediaapp/socialmediaapp/settings.py
+++ socialmediaapp/socialmediaapp/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  
         'NAME': 'socialdatabase',  
         'USER': 'root',  
-        'PASSWORD': '135135',
+        'PASSWORD': '******',
         'HOST': '127.0.0.1',
         'PORT': '3306',  
         'OPTIONS': {  
